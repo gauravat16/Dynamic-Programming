4cf888af9fee88deeb3536e0a266ba30da035908
Gaurav Sharma <gauravat16@users.noreply.github.com>
2017-02-01 15:21:41 +0530

    Update MatrixChainMultiplication.java

--- MatrixChainMultiplication.java
+++ MatrixChainMultiplication.java
@@ -1,25 +1,28 @@
 package clrs;
 
 public class MatrixChainMultiplication {
+	static int[][] save;
 
 	static int matrixChainMulp(int[] inp) {
 		int size = inp.length; // We take the size of the input ie. we include
-				      // the 0th col and row but we wont use it.
+		// the 0th col and row but we wont use it.
 
-		int[][] scalarMulpMatrix = new int[size][size]; // 0,1,2,3 (0th wont be used)
+		int[][] scalarMulpMatrix = new int[size][size]; // 0,1,2,3 (0th wont be
+														// used)
 
-		int[][] save = new int[size][size];
 		for (int i = 1; i < size; i++) {
 			scalarMulpMatrix[i][i] = 0; // Putting all zeros in diagonals ie. no
 										// multiplicaton in case of that matrix
 										// itself ie length = 1
 		}
 
-		for (int l = 2; l < size; l++) { // Here we iterate between max length of chain 2 -> as 1
+		for (int l = 2; l < size; l++) { // Here we iterate between max length
+											// of chain 2 -> as 1 is trivial
 
-			for (int i = 1; i < size - l + 1; i++) { // now we select which  matrix we want to  start with
-														
-														
+			for (int i = 1; i < size - l + 1; i++) { // now we select which
+														// matrix we want to
+														// start with
+
 				int j = i + l - 1; // here we select the matrix we want to
 									// multiply with - we add l or length
 									// because it moves as a window of l length
@@ -30,9 +33,11 @@ public class MatrixChainMultiplication {
 
 				for (int k = i; k <= j - 1; k++) { // Traversing through all
 													// possible division points
-					int mulps = scalarMulpMatrix[i][k] + scalarMulpMatrix[k + 1][j] + inp[i - 1] * inp[k] * inp[j]; // Divided on k,so. i->k,k+1->j, + cost of multiplying these two
-																								 
-																													
+					int mulps = scalarMulpMatrix[i][k]
+							+ scalarMulpMatrix[k + 1][j] + inp[i - 1] * inp[k]
+							* inp[j]; // Divided on k,so. i->k,k+1->j, + cost of
+										// multiplying these two
+
 					if (mulps < scalarMulpMatrix[i][j]) {
 						scalarMulpMatrix[i][j] = mulps;
 						save[i][j] = k;
@@ -51,10 +56,24 @@ public class MatrixChainMultiplication {
 
 	}
 
+	static void optimalParenthesis(int i, int j) {
+		if (i == j) {
+			System.out.print("A" + i);
+		} else {
+			System.out.print("(");
+			optimalParenthesis(i, save[i][j]); // from i to the division pt
+			optimalParenthesis(save[i][j] + 1, j); // from division pt+1 to j
+			System.out.print(")");
+		}
+	}
+
 	public static void main(String[] args) {
 		int[] input = { 30, 35, 15, 5, 10, 20, 25 };
 
+		save = new int[input.length][input.length];
 		System.out.println(matrixChainMulp(input));
+		optimalParenthesis(1, 6);
+
 	}
 
 }
